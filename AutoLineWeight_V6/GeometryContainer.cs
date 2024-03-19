016a13e3312c54213b0f5adc793eaeaae12460a9
CXu0607 <gx7@rice.edu>
2024-03-18 23:47:45 -0400

    updating geometry container...

--- AutoLineWeight_V6/GeometryContainer.cs
+++ AutoLineWeight_V6/GeometryContainer.cs
@@ -15,12 +15,65 @@ namespace AutoLineWeight_V6
     {
         public List<Brep> breps = new List<Brep>();
         public List<Mesh> meshes = new List<Mesh>();
+        public List<Curve> curves = new List<Curve>();
+        public List<InstanceObject> blocks = new List<InstanceObject>();
+        public List<string> blockIds = new List<string>();
 
         public GeometryContainer(ObjRef[] objRefs) 
         { 
             foreach(ObjRef objRef in objRefs)
             {
+                Brep brep = objRef.Brep();
+                if (brep != null)
+                {
+                    breps.Add(brep);
+                    continue;
+                }
 
+                Mesh mesh = objRef.Mesh();
+                if (mesh != null)
+                {
+                    meshes.Add(mesh);
+                    continue;
+                }
+
+                Curve crv = objRef.Curve();
+                if (crv != null)
+                {
+                    curves.Add(crv);
+                    continue;
+                }
+
+                RhinoObject obj = objRef?.Object();
+                if (obj.ObjectType == ObjectType.InstanceReference)
+                {
+                    InstanceObject iref = obj as InstanceObject;
+                    Guid id = iref.Id;
+                    if (blockIds.Contains(id.ToString())) { continue; }
+                    blockIds.Add(id.ToString());
+                    blocks.Add(iref);
+                }
+            }
+        }
+
+        public GeometryContainer(RhinoObject[] objs)
+        {
+            foreach(RhinoObject obj in objs)
+            {
+                if (obj.ObjectType == ObjectType.Brep)
+                {
+                    breps.Add((Brep)obj.Geometry);
+                }
+
+                if (obj.ObjectType == ObjectType.Curve)
+                {
+                    curves.Add((Curve)obj.Geometry);
+                }
+
+                if (obj.ObjectType == ObjectType.Mesh)
+                {
+                    curves.Add((Curve)obj.Geometry);
+                }
             }
         }
     }
